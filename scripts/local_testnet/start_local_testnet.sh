fb2fffdbe19af1bd84fe13638e47514a640fc8fe
Jimmy Chen <jchen.tc@gmail.com>
2022-12-08 14:13:37 +1100

    fix incorrect geth port

--- scripts/local_testnet/start_local_testnet.sh
+++ scripts/local_testnet/start_local_testnet.sh
@@ -134,7 +134,7 @@ for (( bn=1; bn<=$BN_COUNT; bn++ )); do
     execute_command_add_PID json_snoop_$bn.log json_rpc_snoop -p $((EL_base_auth_http + $bn)) -b 0.0.0.0 http://localhost:$((EL_base_auth_http + $bn + 10))
     secret=$DATADIR/geth_datadir$bn/geth/jwtsecret
     echo $secret
-    execute_command_add_PID beacon_node_$bn.log ./beacon_node.sh $SAS -d $DEBUG_LEVEL $DATADIR/node_$bn $((BN_udp_tcp_base + $bn)) $((BN_http_port_base + $bn)) http://localhost:$((EL_base_auth_http + $bn)) $secret
+    execute_command_add_PID beacon_node_$bn.log ./beacon_node.sh $SAS -d $DEBUG_LEVEL $DATADIR/node_$bn $((BN_udp_tcp_base + $bn)) $((BN_http_port_base + $bn)) http://localhost:$((EL_base_auth_http + $bn + 10)) $secret
 done
 
 # Start requested number of validator clients
